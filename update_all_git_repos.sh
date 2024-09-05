b719ff9ed77e4ae19f81e8d07f88abd7ebc1c534
Amin Debian-Lenovo <amin.nouri1997@gmail.com>
2024-09-05 01:24:36 -0400

    routine sync via script

--- update_all_git_repos.sh
+++ update_all_git_repos.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
-per
+eval $(ssh-agent)
+ssh-add
+cd $PERSONAL
 git fetch
 git diff --stat
 git add .
 git commit -m "routine sync via script"
 git push
-back
-dot
+cd $OLDPWD
+cd $MY_DOTFILES
 git fetch
 git diff --stat
 git add .
 git commit -m "routine sync via script"
 git push
-back
-code && cd Scripts
+cd $OLDPWD
+cd "$CODESPACE/Scripts"
 git fetch
 git diff --stat
 git add .
 git commit -m "routine sync via script"
 git push
-back
+cd $OLDPWD
