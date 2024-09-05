86d9b0bbe7f1ed24559ea07afdb066f8e40842b6
Amin Debian-Lenovo <amin.nouri1997@gmail.com>
2024-09-05 01:19:13 -0400

    routine sync via script

---
+++ update_all_git_repos.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+per
+git fetch
+git diff --stat
+git add .
+git commit -m "routine sync via script"
+git push
+back
+dot
+git fetch
+git diff --stat
+git add .
+git commit -m "routine sync via script"
+git push
+back
+code && cd Scripts
+git fetch
+git diff --stat
+git add .
+git commit -m "routine sync via script"
+git push
+back
