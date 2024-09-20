df14769fb91c114fa3b8ed3e0fd65a2543fe0264
Amin Debian-Lenovo <amin.nouri1997@gmail.com>
2024-09-20 14:44:07 -0400

    routine sync via script

---
+++ jentry_add.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# idea: you run this on an entry, check it exists and all, asks you for the details of what you want to add.
+# start time will be assumed from last entry.
+#   print out what happened last for user so they know
+# ask how long this new interval is > store and calculate new end time
+# ask if one or more activities were performed during this interval
+# if one then just add the activity attribute and ask what the activity was
+# else end the tag, start a while loop and ask what the first activity is
+# in either case you store the activity name and ask if they want to provide a description
+#   in all likelihood I can prolly just enter while it's empty and use that instead of the overt asking
+# if ya store description and make tag
+# if it was multiple activities make it an activity tag and ask for the next. otherwise it's an interval and you just close that.
+# end with multiple when they enter empty
+
+
