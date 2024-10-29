7076aa14bc32e305f109e60bcd4d8a1621c673f2
Amin Debian-Lenovo <amin.nouri1997@gmail.com>
2024-10-29 00:25:57 -0400

    routine sync via script

--- sj_StartJournal.sh
+++ sj_StartJournal.sh
@@ -37,7 +37,7 @@ echo '### Dream:' >> $file_name &&
 echo '##### Hygiene: Shower [ ] Shampoo [ ]  Brow [ ]  Cheeks [ ]  Moisturizer [ ] Beard [ ] Floss [ ] Teeth { } { } Calluses [ ] Nails [ ] Trimming { } { } ' >> $file_name &&
 echo '### Finances:' >> $file_name &&
 echo '### Files:' >> $file_name &&
-find ~ \( -path */.git -o -path ~/.cache -o -path ~/.local -o -path ~/.config -o -path ~/.mozilla \) -prune -o -newerct "$(date -d '-1 day' '%Y/%m/%d 6:00:00')" >> $file_name &&
+find ~ \( -path */.git -o -path ~/.cache -o -path ~/.local -o -path ~/.config -o -path ~/.mozilla \) -prune -o -newerct "$(date -d '-1 day' '+%Y/%m/%d 6:00:00')" >> $file_name &&
 echo '### Thoughts:' >> $file_name &&
 nvim "$file_name"
 cd $OLDPWD
