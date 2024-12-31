4fea17b851bd4899c5476d2a760116d22d17e4a1
Amin Debian-Lenovo <amin.nouri1997@gmail.com>
2024-12-31 11:01:03 -0500

    routine sync via script

--- Command_Histogram/date_diff.sh
+++ Command_Histogram/date_diff.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-string1="12:55:35 AM"
-string2="12:17:52 PM"
-string3="10:55:35 AM"
-string4="04:17:52 PM"
+# 26 Jun 2024 01:39:09 AM EDT
+string="26 Jun 2024 01:39:09 AM EDT"
+echo "$string" | awk -v d=1 -v m=2 '{print $d $m}'
 
 function to24 {
-    # [[ "$1" =~ ^(:digit:):(:digit:):(:digit:) AM$ ]] && echo "AM"
     if [ "${1:0-2}" = "AM" ]; then
 	[ "${1:0:2}" = "12" ] && echo "00${1:2:0-3}" || echo "${1:0:0-3}"
     elif [ "${1:0-2}" = "PM" ]; then
@@ -14,10 +12,11 @@ function to24 {
 	hour=$(("${1:0:2}" + 12 ))
 	[ "${1:0:2}" = "12" ] && echo "${1:0:0-3}" || echo "$hour${1:2:0-3}"
     else
-	echo "false"
+	echo "MERIDIEM_error: input is not of expected format, last two characters are not indicative of Meridiem"
+	exit 1
     fi
 }
-
+#need to handle what to do when first two are not hour.
 
 echo "$string1:$string2" | awk -F ':' '{print ($6-$3)+($5-$2)*60+($4-$1)*3600}'
 to24 "$string1"
@@ -28,4 +27,3 @@ to24 "$string4"
 # screw the ultra-generality for years
 # done with hour:min:sec diff
 # need to take am/pm into account
-# 26 Jun 2024 01:39:09 AM EDT
